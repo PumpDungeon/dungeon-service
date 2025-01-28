f470cc4fcfc6a0b825ad5feddffc5399d464c81d
Atomox_ <aoxresell@gmail.com>
2025-01-28 10:42:04 +0100

    done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const {generateMap} = require("./helpers/generateMap");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/generate', (req, res) => {
     const dungeon = generateMap();
@@ -12,6 +14,20 @@ app.get('/api/generate', (req, res) => {
     return res.status(200).json({
         map: dungeon
     })
+});
+
+app.post('/api/move', (req, res) => {
+    const {map, position} = req.body
+    if (map[position.x][position.y] === 2) {
+        return res.status(200).send({
+            message: 'Not possible'
+        })
+    }
+
+    return res.status(200).send({
+        fight: Math.random() >= 0.8,
+        position
+    });
 });
 
 const PORT = process.env.PORT || 3000;
