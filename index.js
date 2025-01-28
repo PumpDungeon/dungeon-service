895eaa78b0380f47d87bf16831021013e0ba14a5
Slynax <remlouplay3@gmail.com>
2025-01-28 10:37:29 +0100

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const {generateMap} = require("./helpers/generateMap");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({}));
 
 app.get('/api/generate', (req, res) => {
     const dungeon = generateMap();
